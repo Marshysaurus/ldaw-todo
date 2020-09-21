614ca8f51f3d6c3b312acd8ab20d780512a3c871
Marshysaurus <ryuzaki.orozco@gmail.com>
2020-09-21 03:09:44 -0500

    Project upload

---
+++ controllers/TasksController.js
@@ -0,0 +1,51 @@
+const Task = require("../models/Task");
+
+exports.store = (req, res) => {
+  let task = {};
+  task.description = req.body.description;
+  Task.create(task).then((id) => {
+    if (req.xhr || req.headers.accept.indexOf("json") >= 0) {
+      Task.find(id).then((task) => {
+        res.json(task);
+      });
+    } else {
+      res.redirect("/");
+    }
+  });
+};
+
+exports.done = (req, res) => {
+  Task.find(req.params.id)
+    .then((task) => {
+      if (task) {
+        return Task.done(req.params.id);
+      }
+    })
+    .then(() => {
+      if (req.xhr || req.headers.accept.indexOf("json") >= 0) {
+        Task.find(req.params.id).then((task) => {
+          res.json(task);
+        });
+      } else {
+        res.redirect("/");
+      }
+    });
+};
+
+exports.delete = (req, res) => {
+  Task.find(req.params.id)
+    .then((task) => {
+      console.log("owo");
+      console.log(task);
+      if (task) {
+        return Task.delete(req.params.id);
+      }
+    })
+    .then(() => {
+      if (req.xhr || req.headers.accept.indexOf("json") >= 0) {
+        res.json({ id: req.params.id });
+      } else {
+        res.redirect("/");
+      }
+    });
+};
